a0a54b4fdc053a1834facbea520e7503bf48e7e5
sgillen <seangillen1887@gmail.com>
2017-09-13 19:17:56 -0700

    added back in better Jacobian finding algo

--- cg_torso_files/cg_torso_find_limit_cycle.m
+++ cg_torso_files/cg_torso_find_limit_cycle.m
@@ -25,17 +25,26 @@ options.StepTolerance = 1e-7;
 options.MaxFunctionEvaluations = 1e4;
 
 %% Can use either "fmincon" or "lsqnonline" -- or another fn
-Xfixed = fmincon(@cg_torso_LCcost,Xinit,[],[],[],[],[],[],[],options); %,);
+Xfixed = fmincon(@cg_torso_LCcost,Xinit,[],[],[],[],[],[],[],options) %,);
 %Xfixed = lsqnonlin(@cg_torso_LCcost,Xinit,[],[],options); %,[],[],[],[],[],[],[],options);
 
 Xerr = max(abs(Xfixed - cg_torso_step(Xfixed)))
 
+
+
 damt = 1e-4;
 J = zeros(6,6);
+
 for n=1:6
     d = zeros(6,1); d(n)=damt;
-    xtemp = cg_torso_step(Xfixed+d);
-    J(:,n) = (1/damt)*(xtemp-Xfixed);
+    xtemp = cg_torso_step(Xfixed + d);
+    xtemp2 = cg_torso_step(Xfixed - d);
+    xnom = cg_torso_step(Xfixed);
+    %J(:,n) = (1/damt)*(xtemp-Xfixed);
+    %J(:,n) = (1/damt)*(xtemp-xnom); % blue circles with dashed line
+    J(:,n) = (1/(2*damt))*(xtemp-xtemp2); % green triangles with '-.' line
+    
+    
 end
 [eivec,eival] = eig(J);
 eival = diag(eival)
