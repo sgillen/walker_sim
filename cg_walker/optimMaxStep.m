f9da30ff0a9f9c8f5b23fe657faaf6db89419d8f
sgillen <seangillen1887@gmail.com>
2018-04-27 20:12:40 -0700

    generating 2d parameter sweep, mass and th3_ref

--- cg_walker/optimMaxStep.m
+++ cg_walker/optimMaxStep.m
@@ -11,16 +11,28 @@ function [step_height] = optimMaxStep(in)
 
 
 walker = CGTorsoWalker();
-walker.controller.th3_ref = 0;
+%walker.controller.th3_ref = 0;
 
 %walker.controller.th2_ref = in(1) * 2*pi;
-%walker.controller.th3_ref = in(2) * 2*pi; 
+walker.controller.th3_ref = in(1) * 2*pi; 
 
 %walker.controller.kp2 = in(3) * 1000;
 %walker.controller.kd2 = in(4) * 100; 
 
-walker.controller.kp3 = in(1) * 1000; %one is the max input from the solver so 1000 is the max kp3 we will allow.
-walker.controller.kd3 = in(2) * 100; 
+walker.controller.kp3 = in(2) * 1000; %one is the max input from the solver so 1000 is the max kp3 we will allow.
+walker.controller.kd3 = in(3) * 100; 
 
+  
+walker.m3 = in(4)*30;
+walker.m2 = (30 - walker.m3)/2;
+walker.m1 = walker.m2;
+
+walker.J1 = 1/3*walker.m1*walker.L1^2
+walker.J2 = 1/3*walker.m2*walker.L2^2
+walker.J3 = 1/3*walker.m3*walker.L3^2
+
+
+%this just makes the maximum step up into a minimization problem
+%The 100 is just because some solvers don't like starting at zero. 
 step_height = 100 - maxStep(walker,1);
 
