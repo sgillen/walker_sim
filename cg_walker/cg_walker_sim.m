7a894159d8bbcc307c097c8ed7f5be5d68d012c2
sgillen <sgillen@umail.ucsb.edu>
2018-05-01 17:32:14 -0700

    re-ran mass and th3_ref sweep

--- cg_walker/cg_walker_sim.m
+++ cg_walker/cg_walker_sim.m
@@ -15,43 +15,45 @@
 %format compact
 grid_size = 25; 
 
+Pinit = zeros(grid_size,4);
+Pfinal =-1.*ones(grid_size,4);
 
-
-for i = 1:grid_size
-    options = optimoptions('fmincon');
-    options = optimoptions(options, 'OptimalityTolerance', 1e-4);
-    Pinit(i) = [.5 + rand/2 ,.5 +rand/2 ,.5 + rand/2 , .5 + rand/2];
-    [walker(i), step_height(i)] = fmincon(@(in)optimMaxStep(in),Pinit(i),[],[],[],[],[],[], [] ,options)
-    
-
-end
-    
-
+% for i = 1:grid_size
+%     i
+%     options = optimoptions('fmincon');
+%     options = optimoptions(options, 'OptimalityTolerance', 1e-4);
+%     Pinit(i,:) = [.5+rand/2 , .5+rand/2 , .5+rand/2 , .5+rand/2];
+%     [Pfinal(i,:), step_height(i)] = fmincon(@(in)optimMaxStep(in),Pinit(i,:),[],[],[],[],[],[], [] ,options);
+%     
 % 
-% walker(grid_size,grid_size) = CGTorsoWalker();
-% 
-% step_height= -1.*ones(grid_size,grid_size);
-% 
-% 
-% parfor i = 1:grid_size
-%     for j = 1:grid_size
-%         (i-1)*grid_size + (j-1)
-%         %walker[i,j] = CGTorsoWalker;
-%         walker(i,j).controller.th3_ref = (j/grid_size)*(80*pi/180) - 40*pi/180;
-%         
-%         walker(i,j).m3 = i/grid_size*30;
-%         walker(i,j).m2 = (30 - walker(i,j).m3)/2;
-%         walker(i,j).m1 = walker(i,j).m2;
-%         
-%         walker(i,j).J1 = 1/3*walker(i,j).m1*walker(i,j).L1^2
-%         walker(i,j).J2 = 1/3*walker(i,j).m2*walker(i,j).L2^2
-%         walker(i,j).J3 = 1/3*walker(i,j).m3*walker(i,j).L3^2
-% 
-%         %walker(i,j).controller.kp3 = i/grid_size*1000;
-%         %walker(i,j).controller.kd3 = j/grid_size*200;
-%         step_height(i,j) = maxStep(walker(i,j),1);    
-%     end
 % end
+    
+
+
+walker(grid_size,grid_size) = CGTorsoWalker();
+
+step_height= -1.*ones(grid_size,grid_size);
+
+
+parfor i = 1:grid_size
+    for j = 1:grid_size
+        (i-1)*grid_size + (j-1)
+        %walker[i,j] = CGTorsoWalker;
+        walker(i,j).controller.th3_ref = (j/grid_size)*(80*pi/180) - 40*pi/180;
+        
+        walker(i,j).m3 = i/grid_size*30;
+        walker(i,j).m2 = (30 - walker(i,j).m3)/2;
+        walker(i,j).m1 = walker(i,j).m2;
+        
+        walker(i,j).J1 = 1/3*walker(i,j).m1*walker(i,j).L1^2
+        walker(i,j).J2 = 1/3*walker(i,j).m2*walker(i,j).L2^2
+        walker(i,j).J3 = 1/3*walker(i,j).m3*walker(i,j).L3^2
+
+        %walker(i,j).controller.kp3 = i/grid_size*1000;
+        %walker(i,j).controller.kd3 = j/grid_size*200;
+        step_height(i,j) = maxStep(walker(i,j),1);    
+    end
+end
 
 
 % 
