63f7cf4c02cc86d376a5c1f8eb29a3c74098293c
sgillen <sgillen@umail.ucsb.edu>
2018-05-01 17:54:49 -0700

    running th2 th3 sweep

--- cg_walker/cg_walker_sim.m
+++ cg_walker/cg_walker_sim.m
@@ -34,15 +34,43 @@ walker(grid_size,grid_size) = CGTorsoWalker();
 
 step_height= -1.*ones(grid_size,grid_size);
 
+% 
+% parfor i = 1:grid_size
+%     for j = 1:grid_size
+%         (i-1)*grid_size + (j-1)
+%         %walker[i,j] = CGTorsoWalker;
+%         walker(i,j).controller.th3_ref = (j/grid_size)*(80*pi/180) - 40*pi/180;
+%         
+%         walker(i,j).m3 = i/grid_size*30;
+%         walker(i,j).m2 = (30 - walker(i,j).m3)/2;
+%         walker(i,j).m1 = walker(i,j).m2;
+%         
+%         walker(i,j).J1 = 1/3*walker(i,j).m1*walker(i,j).L1^2
+%         walker(i,j).J2 = 1/3*walker(i,j).m2*walker(i,j).L2^2
+%         walker(i,j).J3 = 1/3*walker(i,j).m3*walker(i,j).L3^2
+% 
+%         %walker(i,j).controller.kp3 = i/grid_size*1000;
+%         %walker(i,j).controller.kd3 = j/grid_size*200;
+%         step_height(i,j) = maxStep(walker(i,j),1);    
+%     end
+% end
+% 
+
 
 parfor i = 1:grid_size
     for j = 1:grid_size
-        (i-1)*grid_size + (j-1)
-        %walker[i,j] = CGTorsoWalker;
-        walker(i,j).controller.th3_ref = (j/grid_size)*(80*pi/180) - 40*pi/180;
         
-        walker(i,j).m3 = i/grid_size*30;
-        walker(i,j).m2 = (30 - walker(i,j).m3)/2;
+        
+        (i-1)*grid_size + (j-1)
+        
+        walker(i,j).controller.CType = 1; %absolute reference for th3, rel for th2
+        %walker[i,j] = CGTorsoWalker;
+        walker(i,j).controller.th3_ref = (j/grid_size)*(80*pi/180);
+        walker(i,j).controller.th2_ref = ((180) + (i/grid_size)*90)*pi/180;
+        
+        
+        walker(i,j).m3 = 15.6; 
+        walker(i,j).m2 = 7.2;
         walker(i,j).m1 = walker(i,j).m2;
         
         walker(i,j).J1 = 1/3*walker(i,j).m1*walker(i,j).L1^2
