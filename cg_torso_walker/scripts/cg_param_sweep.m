cd9d85b8ce7f93c602288163a39d86fd36cde6b6
sgillen <seangillen1887@gmail.com>
2018-07-23 17:26:45 -0700

    trying mass sweep run again

--- cg_torso_walker/scripts/cg_param_sweep.m
+++ cg_torso_walker/scripts/cg_param_sweep.m
@@ -26,17 +26,17 @@ for n = 1:size(noise_levels,2)
         
             
             walker(i,j,n).th3_ref = 20*pi/180;
-            walker(i,j,n).kp3 = i/grid_size*550 + 50;
+            %walker(i,j,n).kp3 = i/grid_size*550 + 50;
         
             %walker(i,j).L3c = j/grid_size*.9 + .1;
             
-            %walker(i,j,n).m3 = i/grid_size*15 + 7.5;
-            %walker(i,j,n).m2 = (30 - walker(i,j,n).m3)/2;
-            %walker(i,j,n).m1 = walker(i,j,n).m2;
+            walker(i,j,n).m3 = i/grid_size*15 + 7.5;
+            walker(i,j,n).m2 = (30 - walker(i,j,n).m3)/2;
+            walker(i,j,n).m1 = walker(i,j,n).m2;
             
-            %walker(i,j,n).J1 = 1/3*walker(i,j,n).m1*walker(i,j,n).L1^2;
-            %walker(i,j,n).J2 = 1/3*walker(i,j,n).m2*walker(i,j,n).L2^2;
-            %walker(i,j,n).J3 = 1/3*walker(i,j,n).m3*walker(i,j,n).L3^2;
+            walker(i,j,n).J1 = 1/3*walker(i,j,n).m1*walker(i,j,n).L1c^2;
+            walker(i,j,n).J2 = 1/3*walker(i,j,n).m2*walker(i,j,n).L2c^2;
+            walker(i,j,n).J3 = 1/3*walker(i,j,n).m3*walker(i,j,n).L3c^2;
             
             
             step_height(i,j,n) = maxStep(walker(i,j,n),1);
